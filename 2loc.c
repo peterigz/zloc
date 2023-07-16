43bd359fdd837bade9904c16ac77785f74d3ce1e
Peter Rigby <contact@peterrigby.me.uk>
2023-07-16 19:57:52 +0100

    Basic structure setup

--- 2loc.c
+++ 2loc.c
@@ -10,67 +10,70 @@
 #define tfxMin(a, b) (((a) < (b)) ? (a) : (b))
 #define tfxMax(a, b) (((a) > (b)) ? (a) : (b))
 
-typedef unsigned int tlsf_index;
+typedef unsigned int TLocIndex;
 typedef unsigned int u32;
-#ifndef SECOND_LEVEL_SUBDIVISIONS
-#define SECOND_LEVEL_SUBDIVISIONS 4
-#endif
-#define MIN_BLOCK_SIZE 16
-const u32 second_level_indexes = 1 << SECOND_LEVEL_SUBDIVISIONS;
 
-enum tlsf_constants {
-	MEMORY_ALIGNMENT = 8,
+#ifndef SECOND_LEVEL_SUBDIVISIONS_LOG2
+#define SECOND_LEVEL_SUBDIVISIONS_LOG2 5
+#endif
+
+#ifndef MEMORY_ALIGNMENT_LOG2
+#define MEMORY_ALIGNMENT_LOG2 3
+#endif
+
+#define MIN_BLOCK_SIZE 16
+const u32 second_level_indexes = 1 << SECOND_LEVEL_SUBDIVISIONS_LOG2;
+
+enum tloc_constants {
+	MEMORY_ALIGNMENT = 1 << MEMORY_ALIGNMENT_LOG2,
 	MINIMUM_BLOCK_SIZE = 16,
-	SECOND_LEVEL_INDEX_COUNT = 1 << SECOND_LEVEL_SUBDIVISIONS
+	SECOND_LEVEL_INDEX_COUNT = 1 << SECOND_LEVEL_SUBDIVISIONS_LOG2,
+	FIRST_LEVEL_INDEX_MAX = 32,
+	FIRST_LEVEL_INDEX_SHIFT = SECOND_LEVEL_SUBDIVISIONS_LOG2 + MEMORY_ALIGNMENT_LOG2,
+	FIRST_LEVEL_INDEX_COUNT = FIRST_LEVEL_INDEX_MAX - FIRST_LEVEL_INDEX_SHIFT + 1,
+	BLOCK_IS_FREE = 1 << 0,
+	PREV_BLOCK_IS_FREE = 1 << 1
 };
 
-typedef struct tlsf {
+typedef struct TLoc {
 	u32 first_level_index_count;
 	u32 first_level_bitmap;
 	u32 second_level_indexes[SECOND_LEVEL_INDEX_COUNT];
-} tlsf;
+	block_header_t* blocks[FL_INDEX_COUNT][SECOND_LEVEL_INDEX_COUNT];
+} TLoc;
 
-tlsf *InitialiseMemoryPool(void *memory, u32 size) {
-	tlsf *parameters = (tlsf*)memory;
-	u32 fli_index;
-	memset(parameters, 0, sizeof(tlsf));
+TLoc *InitialiseMemoryPool(void *memory, u32 size) {
+	TLoc *parameters = (TLoc*)memory;
+	TLocIndex fli_index;
+	memset(parameters, 0, sizeof(TLoc));
 	_BitScanReverse(&fli_index, size);
 	parameters->first_level_index_count = tfxMin(fli_index, 31);
 }
 
-struct header {
-	unsigned int size;		//
-};
+typedef struct TLocHeader {
+	struct TLocHeader *prev_physical_block;
+	size_t size;
+	struct TLocHeader *prev_free_block;
+	struct TLocHeader *next_free_block;
+} TLocHeader;
 
-inline u32 calculate_fli(u32 size) {
-	u32 fli;
-	tfxMin(_BitScanReverse(&fli, size), 31);
-	return fli;
-}
-
-inline tlsf_index calculate_first_level_index(u32 size) {
-	tlsf_index index;
+inline TLocIndex calculate_first_level_index(u32 size) {
+	TLocIndex index;
 	_BitScanReverse(&index, size);
 	return index;
 }
 
-inline tlsf_index calculate_second_level_index(u32 size, tlsf_index first_level_index) {
+inline TLocIndex calculate_second_level_index(u32 size, TLocIndex first_level_index) {
 	size = size & ~(1 << first_level_index);
-	size = size >> (first_level_index - SECOND_LEVEL_SUBDIVISIONS);
+	size = size >> (first_level_index - SECOND_LEVEL_SUBDIVISIONS_LOG2);
 	return size;
 }
 
 int main()
 {
 
-	size_t s = 4;
-	while (s <= tfxGigabyte(1)) {
-		printf("Size: %zu = FL: %f\n", s, (float)log2((double)s));
-		s = s << 1;
-	}
-
-	tlsf_index fi = calculate_first_level_index(300);
-	tlsf_index si = calculate_second_level_index(300, fi);
+	TLocIndex fi = calculate_first_level_index(460);
+	TLocIndex si = calculate_second_level_index(460, fi);
 		
     printf("F: %u, S: %u\n", fi, si);
 	
